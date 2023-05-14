7580880afd78b2ea11b99bc5a339be015b7db143
davidovits1 <david.o3111@gmail.com>
2023-05-15 02:11:25 +0300

    add ctor dtor and declaration of a function legalMoovs

--- include/Piece.h
+++ include/Piece.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Color.h"
+#include <vector>
+
+class Piece
+{
+protected:
+	Color color;
+	char kind;
+
+public:
+	Piece(Color c = white, char k = ' ');
+	virtual ~Piece() {};
+	
+	virtual std::vector<std::vector<bool>> legalMoovs(int pawnRow, int pawnCol) = 0;
+};
---
+++ src/Piece.cpp
@@ -0,0 +1,7 @@
+#include "Piece.h"
+
+Piece::Piece(Color c, char k)
+{
+	color = c;
+	kind = k;
+}
